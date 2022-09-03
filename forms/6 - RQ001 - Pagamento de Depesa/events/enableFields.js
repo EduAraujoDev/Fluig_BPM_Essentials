0c5328e2be22c27d78994a487c8eab3b2182f932
Eduardo Araújo <eduardo.araujo0@outlook.com>
2022-09-02 22:55:27 -0300

    Commit quarta aula

--- forms/6 - RQ001 - Pagamento de Depesa/events/enableFields.js
+++ forms/6 - RQ001 - Pagamento de Depesa/events/enableFields.js
@@ -1,3 +1,15 @@
 function enableFields(form){
+    var atividadeAtual = getValue("WKNumState");
+
     form.setEnabled("RQ001_nome_usuario", false);
+
+    if (atividadeAtual == 1 || atividadeAtual == 0) {
+        form.setEnabled("RQ001_data", true);
+    } else {
+        form.setEnabled("RQ001_data", false);
+    }
+
+    if (atividadeAtual == 11) {
+        form.setEnabled("RQ001_valor", true);
+    }
 }
